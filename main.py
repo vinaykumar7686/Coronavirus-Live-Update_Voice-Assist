743e7fbf79e7ebf59e8151b9c1caf4383cb6d58d
Vinay <vinaykumarvk.8866@gmail.com>
2020-07-07 23:52:57 +0530

    Implemented methods to read the data recieved in JSON format.

--- main.py
+++ main.py
@@ -20,8 +20,36 @@ class DataCollect:
     def get_data(self):
         response = requests.get(f"https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data", params = {'api_key': API_KEY})
         self.data = json.loads(response.text)
+    
+    def get_total_cases(self):
+        data =  self.data['total']
+        for content in data:
+            if content['name'] == 'Coronavirus Cases:':
+                return content['values']
+
+    def get_total_deaths(self):
+        data =  self.data['total']
+        for content in data:
+            if content['name'] == 'Deaths:':
+                return content['values']
+
+    def get_total_recovered(self):
+        data =  self.data['total']
+        for content in data:
+            if content['name'] == 'Recovered:':
+                return content['values']
+
+    def get_country_data(self, country):
+        data = self.data['country']
+        for content in data:
+            if content['name'].lower() == country.lower():
+                return content
 
 data = DataCollect(API_KEY, PROJECT_TOKEN)
-print(data.data)
+#print(data.data)
+print(f'Total Cases: {data.get_total_cases()}')
+print(f'Total Recovered: {data.get_total_recovered()}')
+print(f'Total Deaths: {data.get_total_deaths()}')
+data.get_country_data('USA')
 
 
